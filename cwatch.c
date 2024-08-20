e1fce2ec4c2671b488ddeb4f4c865542a580c33d
Kevin Player <kjplaye@gmail.com>
2024-08-20 16:28:10 -0400

    Move variable into local scope

--- cwatch.c
+++ cwatch.c
@@ -52,24 +52,19 @@ int rb_size = 0;
 int *model_color;
 
 void run_command(char *command, char *buffer) {
-  FILE *fp;
   int buffer_size;
 
-  fp = popen(command, "r");
+  FILE *fp = popen(command, "r");
   buffer_size = fread(buffer, 1, max_str - 1, fp);
   buffer[buffer_size] = 0;
   pclose(fp);
 }
 
 int get_color(int index) {
-  int i;
+  if (index == 0) return -1;
+  if (index == 1) return 0;
 
-  if (index == 0)
-    return -1;
-  if (index == 1)
-    return 0;
-
-  for (i = 1; i < NUM_COLORS; i++) {
+  for (int i = 1; i < NUM_COLORS; i++) {
     if ((index > color_pos[i - 1]) && (index <= color_pos[i]))
       return i;
   }
@@ -81,8 +76,6 @@ int get_color(int index) {
 // (x,y)  --> [y][x-y+half_window_size]
 int edit_distance_color(char *model, char *old, int *color, int fill_color,
                         int *difference_flag) {
-  int i, x, y, w, xx, yy, ww, ss, mm, best_i, best_ds, best_mm, best_xx,
-      best_yy, best_ww, len_model, len_old;
   int dx[3] = {1, 0, 1};
   int dy[3] = {1, 1, 0};
   int ds[3] = {MATCH_SCORE, DELETE_SCORE, INSERT_SCORE};
@@ -90,11 +83,11 @@ int edit_distance_color(char *model, char *old, int *color, int fill_color,
 
   *difference_flag = 0;
 
-  len_model = strnlen(model, max_str);
-  len_old = strnlen(old, max_str);
+  int len_model = strnlen(model, max_str);
+  int len_old = strnlen(old, max_str);
   if (len_model == 0 || len_old == 0) {
     // Empty string, bail.
-    for (y = 0; y < len_model; y++)
+    for (int y = 0; y < len_model; y++)
       color[y] = fill_color;
     *difference_flag = 1;
     return STATUS_EMPTY_STRING;
@@ -103,23 +96,28 @@ int edit_distance_color(char *model, char *old, int *color, int fill_color,
   // Forward pass
   score[VINDEX(0, 0)] = 0;
   match[VINDEX(0, 0)] = model[0] == old[0];
-  for (y = 0; y < len_model; y++)
-    for (w = 0; w < window_size; w++) {
-      x = w + y - half_window_size;
+  for (int y = 0; y < len_model; y++)
+    for (int w = 0; w < window_size; w++) {
+      int x = w + y - half_window_size;
       if (x < 0 || x >= len_old)
         continue;
       if (x == 0 && y == 0)
         continue;
-      best_ds = 0;
-      best_i = 0;
-      for (i = 0; i < 3; i++) {
-        xx = x - dx[i];
-        yy = y - dy[i];
-        ww = xx - yy + half_window_size;
+      
+      int best_ds = 0;
+      int best_i = 0;
+      int best_mm = 0;
+      int best_xx = 0;
+      int best_yy = 0;
+      int best_ww = 0;       
+      for (int i = 0; i < 3; i++) {
+        int xx = x - dx[i];
+        int yy = y - dy[i];
+        int ww = xx - yy + half_window_size;
         if (xx < 0 || yy < 0 || ww < 0 || ww >= window_size)
           continue;
-        ss = ds[i];
-        mm = dm[i];
+        int ss = ds[i];
+        int mm = dm[i];
         if (i == 0 && (model[y] != old[x])) {
           ss = ERROR_SCORE;
           mm = 0;
@@ -140,11 +138,11 @@ int edit_distance_color(char *model, char *old, int *color, int fill_color,
     }
 
   // Backward pass
-  for (xx = len_old - 1, yy = len_model - 1; xx > 0 || yy > 0;) {
-    ww = xx - yy + half_window_size;
+  for (int xx = len_old - 1, yy = len_model - 1; xx > 0 || yy > 0;) {
+    int ww = xx - yy + half_window_size;
     if (ww < 0 || ww >= window_size) {
       // window_size too small, bail.
-      for (y = 0; y < len_model; y++)
+      for (int y = 0; y < len_model; y++)
         color[y] = fill_color;
       *difference_flag = 1;
       return STATUS_WINDOW_SIZE_TOO_SMALL;
@@ -155,8 +153,9 @@ int edit_distance_color(char *model, char *old, int *color, int fill_color,
     }
     if (yy == 0 && ww <= half_window_size)
       break;
-    y = prev_y[VINDEX(yy, ww)];
-    x = prev_x[VINDEX(yy, ww)];
+
+    int y = prev_y[VINDEX(yy, ww)];
+    int x = prev_x[VINDEX(yy, ww)];
     yy = y;
     xx = x;
   }
@@ -166,15 +165,11 @@ int edit_distance_color(char *model, char *old, int *color, int fill_color,
 
 void cprint(char *model, int *color, double duration, int status,
             double time_since_most_recent_change) {
-  int i, j;
-  int current_color = -1;
-  time_t tm;
-  char time_str[MAX_STR];
-
   if (clear_terminal)
     printf("\033[?1049h\033[H");
 
-  tm = time(NULL);
+  char time_str[MAX_STR];
+  time_t tm = time(NULL);
   strcpy(time_str, ctime(&tm));
   time_str[strnlen(time_str, max_str) - 1] = 0;
   printf("\033[34m");
@@ -198,7 +193,8 @@ void cprint(char *model, int *color, double duration, int status,
   printf("----------------------------------------------\n");
   printf("\033[0m");
 
-  for (i = 0; i < strnlen(model, max_str); i++) {
+  int current_color = -1;
+  for (int i = 0; i < strnlen(model, max_str); i++) {
     if (color[i] != current_color) {
       if (color[i] == -1) {
         printf("\033[0m");
@@ -222,8 +218,6 @@ double get_seconds() {
 
 void argparse(int argc, char **argv, double *delay, int *max_str,
               int *half_window_size, int *history) {
-  int i;
-
   *delay = DEFAULT_DELAY;
   *max_str = DEFAULT_MAX_STR;
   *half_window_size = DEFAULT_HALF_WINDOW_SIZE;
@@ -253,31 +247,31 @@ void argparse(int argc, char **argv, double *delay, int *max_str,
     exit(1);
   }
 
-  for (i = 1; i < argc - 1; i++) {
+  for (int i = 1; i < argc - 1; i++) {
     if (!strcmp(argv[i], "-c")) {
       clear_terminal = 0;
     }
   }
 
-  for (i = 1; i < argc - 2; i++) {
+  for (int i = 1; i < argc - 2; i++) {
     if (!strcmp(argv[i], "-d")) {
       *delay = atof(argv[i + 1]);
     }
   }
 
-  for (i = 1; i < argc - 2; i++) {
+  for (int i = 1; i < argc - 2; i++) {
     if (!strcmp(argv[i], "-h")) {
       *history = atoi(argv[i + 1]);
     }
   }
 
-  for (i = 1; i < argc - 2; i++) {
+  for (int i = 1; i < argc - 2; i++) {
     if (!strcmp(argv[i], "-s")) {
       *max_str = atoi(argv[i + 1]);
     }
   }
 
-  for (i = 1; i < argc - 2; i++) {
+  for (int i = 1; i < argc - 2; i++) {
     if (!strcmp(argv[i], "-p")) {
       char *picked_color_str = argv[i + 1];
       int j;
@@ -295,7 +289,7 @@ void argparse(int argc, char **argv, double *delay, int *max_str,
     }
   }
 
-  for (i = 1; i < argc - 2; i++) {
+  for (int i = 1; i < argc - 2; i++) {
     if (!strcmp(argv[i], "-w")) {
       *half_window_size = atoi(argv[i + 1]);
     }
@@ -303,14 +297,7 @@ void argparse(int argc, char **argv, double *delay, int *max_str,
 }
 
 int main(int argc, char **argv) {
-  int i, j, h, status;
-  double tic, toc;
   double delay;
-  int difference_flag;
-  double time_since_most_recent_change;
-  double time_since_least_recent_change;
-  double interval;
-
   argparse(argc, argv, &delay, &max_str, &half_window_size, &history);
   window_size = 2 * half_window_size;
 
@@ -338,28 +325,33 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Out of memory\n");
   }
 
-  tic = get_seconds();
+  double tic = get_seconds();
+  double toc = tic;
   while (1) {
+    double time_since_most_recent_change;
+    double time_since_least_recent_change;
+    int status;
     toc = tic;
     tic = get_seconds();
     run_command(argv[argc - 1], &ring_buffer[RINDEX(rb_current, 0)]);
     frame_time[rb_current] = get_seconds();
     if (++rb_size >= history)
       rb_size = history;
-    for (i = 0; i < max_str; i++)
+    for (int i = 0; i < max_str; i++)
       model_color[i] = -1;
     time_since_most_recent_change = 0.0;
     time_since_least_recent_change = 0.0;
-    for (h = rb_size - 1; h > 0; h--) {
-      j = rb_current - h;
+    for (int h = rb_size - 1; h > 0; h--) {
+      int j = rb_current - h;
       if (j < 0)
         j += history;
+
+      int difference_flag;
       status = edit_distance_color(&ring_buffer[RINDEX(rb_current, 0)],
                                    &ring_buffer[RINDEX(j, 0)], model_color,
                                    get_color(h), &difference_flag);
       if (difference_flag) {
-        interval = frame_time[rb_current] - frame_time[j];
-        time_since_most_recent_change = interval;
+        time_since_most_recent_change = frame_time[rb_current] - frame_time[j];
       }
     }
     while (tic - toc < delay) {
