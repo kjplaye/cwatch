aa8264baaa4f56a7c7d20ec4b8c18417e51c68a4
Kevin Player <kjplaye@gmail.com>
2025-10-11 19:23:45 -0400

    lint

--- cwatch.c
+++ cwatch.c
@@ -1,5 +1,3 @@
 int program_main(int argc, char *argv[]);
 
-int main(int argc, char *argv[]) {
-    return program_main(argc, argv);
-}
+int main(int argc, char *argv[]) { return program_main(argc, argv); }
